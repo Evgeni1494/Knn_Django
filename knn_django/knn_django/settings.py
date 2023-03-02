7d56bd7132d66a0262c57ff3837cad328494b19a
Evgeni1494 <yoni1@hotmail.fr>
2023-03-02 09:29:15 +0100

    Du Css

--- knn_django/knn_django/settings.py
+++ knn_django/knn_django/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'predict'
+    'predict',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
